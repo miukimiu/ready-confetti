9293888e72a58c4006cbad37d1ae0ab190a7d802
Elizabet Oliveira <elizabet.oliveira@gmail.com>
2017-11-15 10:59:26 +0000

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,30 @@
+$(function() {
+
+  // init the validator
+  // validator files are included in the download package
+  // otherwise download from http://1000hz.github.io/bootstrap-validator
+
+  $('#contact-form').validator();
+
+  $(document).on('click', 'a[href^="#"]', function(e) {
+      // target element id
+      var id = $(this).attr('href');
+
+      // target element
+      var $id = $(id);
+      if ($id.length === 0) {
+          return;
+      }
+
+      // prevent standard hash navigation (avoid blinking in IE)
+      e.preventDefault();
+
+      // top position relative to the document
+      var pos = $id.offset().top;
+
+      // animated top scrolling
+      $('body, html').animate({scrollTop: pos}, 1200);
+
+
+  });
+});
